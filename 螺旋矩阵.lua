f69425139fd6be5c811b11c33771e9e2deb7911e
Dendrobe <djwboy4@gmail.com>
2012-11-10 14:04:52 +0800

    Correct the name of variables
    Somebody told me that it's not good to declare variables starting with
    underscode

--- 螺旋矩阵.lua
+++ 螺旋矩阵.lua
@@ -1,89 +1,89 @@
-﻿--声明变量
-__theArray = {}
+--声明变量
+theArray = {}
 --初始化矩阵
-function create_Array(__number)
-	for i = 1, __number do
-		__theArray[i] = {}
-		for j = 1,__number do
-		__theArray[i][j] = 0
+function create_Array(number)
+	for i = 1, number do
+		theArray[i] = {}
+		for j = 1,number do
+		theArray[i][j] = 0
 		end
 	end
 end
 --打印矩阵
-function print_Array(__number)
-	local __string = ""
+function print_Array(number)
+	local arrayPrint = ""
 	local k = 1
-	for i = 1, __number do
-		for j = 1, __number do
-			if k ~= __number then
-				__string = __string..tostring(__theArray[i][j]).."\t"
+	for i = 1, number do
+		for j = 1, number do
+			if k ~= number then
+				arrayPrint = arrayPrint..tostring(theArray[i][j]).."\t"
 				k = k + 1
 			else
-				print(__string..tostring(__theArray[i][j]))
+				print(arrayPrint..tostring(theArray[i][j]))
 				k = 1
-				__string = ""
+				arrayPrint = ""
 			end
 		end
 	end
 end
 --内螺旋赋值
-function inward_Spiral(__number)
-	local __i_min = 1
-	local __i_max = __number
-	local __j_min = 1
-	local __j_max = __number
+function inward_Spiral(number)
+	local i_min = 1
+	local i_max = number
+	local j_min = 1
+	local j_max = number
 	local k = 1
-	local __value = 1
+	local count = 1
 	local ical, jcal = 1, 1
 
-	while __value ~= __number^2+1 do
+	while count ~= number^2+1 do
 		if k == 1 then
-			for jcal = __j_min, __j_max do
-				__theArray[ical][jcal] = __value
-				__value = __value + 1
+			for jcal = j_min, j_max do
+				theArray[ical][jcal] = count
+				count = count + 1
 			end
-			jcal = __j_max
-			__i_min = __i_min + 1
+			jcal = j_max
+			i_min = i_min + 1
 			k = 2
 		elseif k == 2 then
-			for ical = __i_min, __i_max do
-				__theArray[ical][jcal] = __value
-				__value = __value + 1
+			for ical = i_min, i_max do
+				theArray[ical][jcal] = count
+				count = count + 1
 			end
-			ical = __i_max
-			__j_max = __j_max - 1
+			ical = i_max
+			j_max = j_max - 1
 			k = 3
 		elseif k == 3 then
-			for jcal = __j_max, __j_min, -1 do
-				__theArray[ical][jcal] = __value
-				__value = __value + 1
+			for jcal = j_max, j_min, -1 do
+				theArray[ical][jcal] = count
+				count = count + 1
 			end
-			jcal = __j_min
-			__i_max = __i_max - 1
+			jcal = j_min
+			i_max = i_max - 1
 			k = 4
 		elseif k == 4 then
-			for ical = __i_max, __i_min, -1 do
-				__theArray[ical][jcal] = __value
-				__value = __value + 1
+			for ical = i_max, i_min, -1 do
+				theArray[ical][jcal] = count
+				count = count + 1
 			end
-			ical = __i_min
-			__j_min = __j_min + 1
+			ical = i_min
+			j_min = j_min + 1
 			k = 1
 		end
 	end
 
-	print_Array(__number)
+	print_Array(number)
 end
 --交互主体
 function main()
-	local __number = nil
+	local number = nil
 	repeat
 		print("Please enter a number:")
-		__number = tonumber(io.read())
-	until __number
-	create_Array(__number)
-	inward_Spiral(__number)
-	--print_Array(__number)
+		number = tonumber(io.read())
+	until number
+	create_Array(number)
+	inward_Spiral(number)
+	--print_Array(number)
 end
 --开始执行
 main()
